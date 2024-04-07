d480172d12f19d93a2acd29554bacd58520fd3df
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-07 17:17:48 +0100

    Completed Exercise 22

--- exercises/exercise_22.py
+++ exercises/exercise_22.py
@@ -1,1 +1,13 @@
 # Your solution to Exercise 22
+def pattern(num):
+    str_num = str(num)
+    if len(str_num) > 1:
+        for _ in range(len(str_num) - 1):
+            num = num // 10
+            print(num)
+    else:
+        print(num)
+
+
+n = int(input())
+pattern(n)
