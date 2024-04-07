eb04784af15fc48d34bcc1e3c962b0e28e6a4ed0
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-07 16:12:58 +0100

    Completed Exercise 19

--- exercises/exercise_19.py
+++ exercises/exercise_19.py
@@ -1,1 +1,11 @@
 # Your solution to Exercise 19
+
+def squares(n):
+    for i in range(10, 100):
+        str_num = str(i)
+        if ((int(str_num[0]) ** 2) + (int(str_num[1]) ** 2)) % n != 0:
+            continue
+        print(i, end=", ")
+
+n = int(input())
+squares(n)
