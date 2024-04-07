5c2f549630804b6e2b3cc0a3ae767f9724a97319
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-07 15:45:50 +0100

    Completed Exercise 12

--- exercises/exercise_12.py
+++ exercises/exercise_12.py
@@ -1,1 +1,11 @@
 # Your solution to Exercise 12
+def sum(n):
+    sum = 0
+    for i in range(100, 1000):
+        if i % n != 0:
+            continue
+        sum += i
+    return sum
+
+n = int(input())
+print(sum(n))
