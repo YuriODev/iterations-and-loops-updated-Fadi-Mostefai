80761f004b1487802bcc0c33949ffd353cc0031d
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-07 15:48:41 +0100

    Completed Exercise 13

--- exercises/exercise_13.py
+++ exercises/exercise_13.py
@@ -1,1 +1,10 @@
 # Your solution to Exercise 13
+def validate(password):
+    pw = int(input())
+    while pw != password:
+        print("Error")
+        pw = int(input())
+    print("Done")
+
+password = int(input())
+validate(password)
