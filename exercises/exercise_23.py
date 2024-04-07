ec38a0c252013a265fa73a3ddcf9998386879bcb
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-07 17:21:28 +0100

    Completed Exercise 23

--- exercises/exercise_23.py
+++ exercises/exercise_23.py
@@ -1,1 +1,17 @@
 # Your solution to Exercise 23
+
+def average():
+    sum = 0
+    count = 0
+    num = int(input())
+    if num == 0:
+        count = 1
+    else:
+        while num != 0:
+            sum += num
+            count += 1
+            num = int(input())
+    av = sum / count
+    return av
+
+print(average())
