f8046b122470f607554683394b0ece788e1ea8b3
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-07 15:50:35 +0100

    Completed Exercise 14

--- exercises/exercise_14.py
+++ exercises/exercise_14.py
@@ -1,1 +1,11 @@
 # Your solution to Exercise 14
+def count(n):
+    sum = 0
+    for _ in range(n):
+        num = int(input())
+        if num == 0:
+            sum += 1
+    return sum
+
+n = int(input())
+print(count(n))
