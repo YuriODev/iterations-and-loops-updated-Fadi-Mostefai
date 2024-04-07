c3ca3c855d0f777dbdf6877a3def4716bb51fcf5
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-07 16:19:04 +0100

    Completed Exercise 21

--- exercises/exercise_21.py
+++ exercises/exercise_21.py
@@ -1,1 +1,15 @@
 # Your solution to Exercise 21
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+def sum_factorial(n):
+    sum = 0
+    for i in range(1, n+1):
+        sum += factorial(i)
+    return sum
+
+n = int(input())
+print(sum_factorial(n))
