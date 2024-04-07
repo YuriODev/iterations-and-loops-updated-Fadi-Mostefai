09111c09dc1cb31e668ade6cd24a87d2663887c3
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-07 16:04:14 +0100

    Completed Exercise 17

--- exercises/exercise_17.py
+++ exercises/exercise_17.py
@@ -1,1 +1,14 @@
 # Your solution to Exercise 17
+def pattern(n, m):
+    for i in range(n):
+        for _ in range(m):
+            print(i, end="\t")
+        print()
+    print()
+            
+
+n = int(input())
+m = int(input())
+pattern(n,m)
+
+#print("0\t0\t0\t\n1\t1\t1\t\n2\t2\t2\t\n3\t3\t3\t\n4\t4\t4\t\n5\t5\t5\t\n")
