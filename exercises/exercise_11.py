bca34fc6e6a3a47be1ffa471bd7df2b24f28adf2
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-07 15:43:41 +0100

    Completed Exercise 11

--- exercises/exercise_11.py
+++ exercises/exercise_11.py
@@ -1,1 +1,14 @@
 # Your solution to Exercise 11
+
+def series(n):
+    sum = 0
+    for i in range(1, n+1):
+        sum += (i)/(i+1)
+    sum = round(sum, 2)
+    if len(str(sum)) == 3:
+        return str(sum) + "0"
+    else:
+        return sum
+
+n = int(input())
+print(series(n))
