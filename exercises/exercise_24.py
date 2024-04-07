708a8b27f9cb1dccbf94698b3cfb09a7fabe23dc
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-07 17:24:07 +0100

    Completed Exercise 24

--- exercises/exercise_24.py
+++ exercises/exercise_24.py
@@ -1,1 +1,11 @@
 # Your solution to Exercise 24
+def even():
+    count = 0
+    num = int(input())
+    while num != 0:
+        if num % 2 == 0:
+            count += 1
+        num = int(input())
+    return count
+
+print(even())
