47a0cc671194796fc20c3935210d0042c1342435
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-08 19:05:04 +0100

    Completed Exercise 41

--- exercises/exercise_41.py
+++ exercises/exercise_41.py
@@ -1,1 +1,17 @@
 # Your solution to Exercise 41
+
+def fib_digit(n):
+    term1 = 0
+    term2 = 1
+    if n == 1:
+        return term2
+    else:
+        term = 0
+        for _ in range(n-1):
+            term = term1 + term2
+            term1 = term2
+            term2 = term
+        return term
+    
+n = int(input())
+print(fib_digit(n))
