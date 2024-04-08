884c001f21c3f417cda80f43abca67c9c69483a7
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-08 17:40:25 +0100

    Completed Exercise 34

--- exercises/exercise_34.py
+++ exercises/exercise_34.py
@@ -1,1 +1,11 @@
 # Your solution to Exercise 34
+def pattern(n):
+    str_num = ""
+    for i in range(1, n+1):
+        for j in range(1, i+1):
+            str_num += str(j)
+        print(str_num)
+        str_num = ""
+
+n = int(input())
+pattern(n)
