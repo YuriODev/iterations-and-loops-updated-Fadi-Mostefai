82881fb0a4e9a66f98fce2d2c9685bc3f4c3b85e
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-08 17:06:45 +0100

    Completed Exercise 31

--- exercises/exercise_31.py
+++ exercises/exercise_31.py
@@ -1,1 +1,12 @@
 # Your solution to Exercise 31
+
+def temp_checker(d):
+    temp = 0
+    for _ in range(d):
+        num = int(input())
+        if num < temp:
+            temp = num
+    return f"{temp}\nYes" if temp < -18 else f"{temp}\nNo"
+
+d = int(input())
+print(temp_checker(d))
