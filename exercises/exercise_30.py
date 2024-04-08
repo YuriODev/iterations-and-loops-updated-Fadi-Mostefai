b6fa8d1d441a65fe08996cb84f13347c8b9b913d
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-08 17:02:59 +0100

    Completed Exercise 30

--- exercises/exercise_30.py
+++ exercises/exercise_30.py
@@ -1,1 +1,11 @@
 # Your solution to Exercise 30
+
+def amoeba(t):
+    num = t // 3
+    sum = 1
+    for _ in range(num):
+        sum = sum * 2
+    return sum
+
+t = int(input())
+print(amoeba(t))
