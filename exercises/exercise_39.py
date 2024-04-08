9773b77dde7300a31b5c7d61339a0968096b5b2d
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-08 18:29:36 +0100

    Completed Exercise 39

--- exercises/exercise_39.py
+++ exercises/exercise_39.py
@@ -1,1 +1,12 @@
 # Your solution to Exercise 39
+
+def digit(n):
+    sum = 0
+    if n < 0:
+        n = n * -1
+    for digit in str(n):
+        sum += int(digit)
+    return sum
+
+n = int(input())
+print(digit(n))
