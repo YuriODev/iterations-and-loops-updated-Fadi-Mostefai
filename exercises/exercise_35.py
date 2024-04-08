583e4f1a88eedc9499f95a62bf2cf3e4da68d103
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-08 18:01:28 +0100

    Completed Exercise 35

--- exercises/exercise_35.py
+++ exercises/exercise_35.py
@@ -1,1 +1,10 @@
 # Your solution to Exercise 35
+def odd(n):
+    upper_bound = (10 ** n) - 1
+    lower_bound = (10 ** (n-1)) - 1
+    for i in range(upper_bound, lower_bound, -2):
+        print(i, end=" ")
+    print()
+
+n = int(input())
+odd(n)
