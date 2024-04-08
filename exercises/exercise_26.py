1c57e25cf91b19ae5b26975d4292b93cf1a2dcfb
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-08 16:45:48 +0100

    Completed Exercise 26

--- exercises/exercise_26.py
+++ exercises/exercise_26.py
@@ -1,1 +1,13 @@
 # Your solution to Exercise 26
+def intsum(n):
+    count = 0
+    for num in range(100, 1000):
+        sum = 0
+        for digit in str(num):
+            sum += int(digit)
+        if sum == n:
+            count += 1
+    return count
+
+n = int(input())
+print(intsum(n))
