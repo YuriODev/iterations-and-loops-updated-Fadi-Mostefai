9aaea6b4fb1ebc6360fd29f5316eeb4997f4cb87
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-08 18:26:54 +0100

    Completed Exercise 38

--- exercises/exercise_38.py
+++ exercises/exercise_38.py
@@ -1,1 +1,21 @@
 # Your solution to Exercise 38
+
+def even_diff(n):
+    small = 0
+    large = 0
+    if len(str(n)) <= 1:
+        return "False"
+    else:
+        for digit in str(n):
+            if int(digit) > large:
+                if small >= large or small == 0:
+                    small = large
+                large = int(digit)
+            elif int(digit) < small:
+                small = int(digit)
+            elif small == 0:
+                small = int(digit)
+        return "True" if (large - small) % 2 == 0 else "False"
+
+n = int(input())
+print(even_diff(n))
