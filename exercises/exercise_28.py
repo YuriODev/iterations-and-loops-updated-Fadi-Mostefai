770a6fd29cf5bf54f5209185a64844ccfe87d91e
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-08 16:56:17 +0100

    Completed Exercise 28

--- exercises/exercise_28.py
+++ exercises/exercise_28.py
@@ -1,1 +1,11 @@
 # Your solution to Exercise 28
+
+def product(a,b):
+    sum = 0
+    for _ in range(a):
+        sum += b
+    return sum
+
+a = int(input())
+b = int(input())
+print(product(a,b))
