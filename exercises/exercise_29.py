91ad9fc4363cc379e5f891076d0a3a04c1f7c0da
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-08 16:59:13 +0100

    Completed Exercise 29

--- exercises/exercise_29.py
+++ exercises/exercise_29.py
@@ -1,1 +1,13 @@
 # Your solution to Exercise 29
+def series():
+    num = int(input())
+    sum1 = num
+    sum2 = num ** 2
+    while sum1 != 0:
+        num = int(input())
+        sum1 += num
+        sum2 += num ** 2
+    return sum2
+
+print(series())
+    
