308705283e46c88517d47ea3ae34fb7c612997d1
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-08 18:12:18 +0100

    Completed Exercise 36

--- exercises/exercise_36.py
+++ exercises/exercise_36.py
@@ -1,1 +1,22 @@
 # Your solution to Exercise 36
+
+def GCD(a, b):
+    while True:
+        if a == 0:
+            return b
+        elif b == 0:
+            return a
+        temp = a
+        a = b
+        b = temp % b
+
+    #if a == 0:
+        #return b
+    #elif b == 0:
+        #return a
+    #else:
+        #return GCD(b, a % b)
+    
+a = int(input())
+b = int(input())
+print(GCD(a,b))
