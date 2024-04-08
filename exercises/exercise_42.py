0859682b9f7a12c8ad778847ea81770a1f423678
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-08 19:11:13 +0100

    Completed Exercise 42

--- exercises/exercise_42.py
+++ exercises/exercise_42.py
@@ -1,1 +1,14 @@
 # Your solution to Exercise 42
+
+def num_seq():
+    num = int(input())
+    prev = num
+    count = 0
+    while num != 0:
+        num = int(input())
+        if prev > num:
+            count += 1
+        prev = num
+    return count
+
+print(num_seq())
