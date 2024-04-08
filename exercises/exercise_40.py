a7b7bc4c81665239fe114aaba704285a125fefb4
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-08 18:34:13 +0100

    Completed Exercise 40

--- exercises/exercise_40.py
+++ exercises/exercise_40.py
@@ -1,1 +1,17 @@
 # Your solution to Exercise 40
+
+def fibonacci(n):
+    fibonacci_seq = "1"
+    term1 = 0
+    term2 = 1
+    while True:
+        term = term1 + term2
+        if term >= n:
+            break
+        fibonacci_seq += " " + str(term)
+        term1 = term2
+        term2 = term
+    return fibonacci_seq
+
+n = int(input())
+print(fibonacci(n))
