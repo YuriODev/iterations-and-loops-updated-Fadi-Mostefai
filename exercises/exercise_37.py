56d1b58845a81c4060801b190e7c226e19775add
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-08 18:16:11 +0100

    Completed Exercise 37

--- exercises/exercise_37.py
+++ exercises/exercise_37.py
@@ -1,1 +1,16 @@
 # Your solution to Exercise 37
+
+def div(a, b):
+    rem = 0
+    count = 0
+    while True:
+        if a - b < 0:
+            rem = a
+            break
+        a -= b
+        count += 1
+    return f"{count} {rem}"
+
+a = int(input())
+b = int(input())
+print(div(a,b))
