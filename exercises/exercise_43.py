64c346dd43e8e525688365b82c49f5f096f86e30
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-08 19:17:14 +0100

    Completed Exercise 43

--- exercises/exercise_43.py
+++ exercises/exercise_43.py
@@ -1,1 +1,16 @@
 # Your solution to Exercise 43
+def sec_largest():
+    num = int(input())
+    second_large = 0
+    large = num
+    while num != 0:
+        num = int(input())
+        if num > large:
+            if second_large == 0:
+                second_large = large
+            large = num
+        elif second_large < num <= large:
+            second_large = num
+    return second_large
+
+print(sec_largest())
