f8028cf8b671b7922034b68346a3501dfeb74161
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-08 16:52:36 +0100

    Completed Exercise 27

--- exercises/exercise_27.py
+++ exercises/exercise_27.py
@@ -1,1 +1,15 @@
 # Your solution to Exercise 27
+
+def picalc(n):
+    Pi = 0
+    count = 1
+    for _ in range(n):
+        if count % 4 == 1:
+            Pi += 4/count
+        elif count % 4 == 3:
+            Pi -= 4/count
+        count += 2
+    return Pi
+
+n = int(input())
+print(picalc(n))
