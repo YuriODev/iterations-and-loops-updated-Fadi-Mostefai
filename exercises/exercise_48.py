f7467c287960cf0797e011be86c02fba3895771b
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-09 17:04:22 +0100

    Completed Exercise 48

--- exercises/exercise_48.py
+++ exercises/exercise_48.py
@@ -1,1 +1,21 @@
 # Your solution to Exercise 48
+def num_pal(n):
+    count = 0
+    num = 1
+    rev_num = 0
+    while num <= n:
+        temp = num
+        while num != 0:
+            last_digit = num % 10
+            rev_num = rev_num * 10 + last_digit
+            #print(f"Num: {num}, rev_num: {rev_num}")
+            num = num // 10
+        if rev_num == temp:
+            count += 1
+        num = temp
+        num += 1
+        rev_num = 0
+    return count
+
+n = int(input())
+print(num_pal(n))
