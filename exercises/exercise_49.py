86193224f2382d3e0f5abfa6e0e7209f09b21713
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-09 17:08:41 +0100

    Completed Exercise 49

--- exercises/exercise_49.py
+++ exercises/exercise_49.py
@@ -1,1 +1,16 @@
 # Your solution to Exercise 49
+
+def pal(a,b):
+    for i in range(a, b+1):
+        num = i
+        rev_num = 0
+        while num != 0:
+            last_digit = num % 10
+            rev_num = rev_num * 10 + last_digit
+            num = num // 10
+        if rev_num == i:
+            print(i, end=" ")
+
+a = int(input())
+b = int(input())
+pal(a,b)
