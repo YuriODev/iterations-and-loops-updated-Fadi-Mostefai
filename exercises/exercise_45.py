c3c74747a4be8ae5b09b76b0a09d33a557c9f298
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-09 14:34:56 +0100

    Completed Exercise 45

--- exercises/exercise_45.py
+++ exercises/exercise_45.py
@@ -1,1 +1,13 @@
 # Your solution to Exercise 45
+def sign_changes():
+    num = int(input())
+    prev = num
+    count = 0
+    while num != 0:
+        num = int(input())
+        if (prev < 0 and num > 0) or (prev > 0 and num < 0):
+            count += 1
+        prev = num
+    return count
+
+print(sign_changes())
