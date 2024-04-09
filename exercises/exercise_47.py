9daab1b9be34e552b65b5950bf0d8e809331bf39
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-09 15:06:24 +0100

    Completed Exercise 47

--- exercises/exercise_47.py
+++ exercises/exercise_47.py
@@ -1,1 +1,24 @@
 # Your solution to Exercise 47
+
+def palindrome(n):
+    last_pal = 1
+    while True:
+        print(last_pal, end=" ")
+        if last_pal < 9:
+            last_pal += 1
+        elif last_pal == 9:
+            last_pal += 2
+        elif 11 <= last_pal < 99:
+            last_pal += 11
+        elif last_pal == 99:
+            last_pal += 2
+        elif ((last_pal // 10) % 10) == 9:
+            last_pal += 11
+        else:
+            last_pal += 10
+        if last_pal >= n:
+            break
+        
+
+n = int(input())
+palindrome(n)
