a9adfa135052a080332c8da9c7c23b1637a88739
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-09 14:32:43 +0100

    Completed Exercise 44

--- exercises/exercise_44.py
+++ exercises/exercise_44.py
@@ -1,1 +1,15 @@
 # Your solution to Exercise 44
+def index():
+    num = int(input())
+    prev = num
+    count = 1
+    i = 0
+    while num != 0:
+        num = int(input())
+        if num >= prev:
+            prev = num
+            i = count
+        count += 1
+    return i
+
+print(index())
