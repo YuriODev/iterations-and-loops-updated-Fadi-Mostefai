b11c1c79f4d8813c2ac7ff5fd9e7a5ffac28f249
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-10 18:32:53 +0100

    Completed Exercise 52

--- exercises/exercise_52.py
+++ exercises/exercise_52.py
@@ -1,1 +1,11 @@
 # Your solution to Exercise 52
+
+def odd_num(a,b):
+    i = (a - 1) * (a % 2 == 0) + a * (a % 2 == 1)
+    while b <= i <= a:
+        print(i, end=" ")
+        i -= 2
+
+a = int(input())
+b = int(input())
+odd_num(a,b)
