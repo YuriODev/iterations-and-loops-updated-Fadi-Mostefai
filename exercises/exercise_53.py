0daf65c2d30d83850e479d718f53e825a9d8c90c
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-10 18:35:00 +0100

    Completed Exercise 53

--- exercises/exercise_53.py
+++ exercises/exercise_53.py
@@ -1,1 +1,10 @@
 # Your solution to Exercise 53
+def even_num(a, b):
+    i = (a + 1) * (a % 2 == 1) + (a) * (a % 2 == 0)
+    while a <= i <= b:
+        print(i, end=" ")
+        i += 2
+
+a = int(input())
+b = int(input())
+even_num(a,b)
