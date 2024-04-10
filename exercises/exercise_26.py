76f3cf2ac1138b9a1a0c6500900040108a353f85
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-10 18:48:30 +0100

    Fixed Exercise 26

--- exercises/exercise_26.py
+++ exercises/exercise_26.py
@@ -3,8 +3,10 @@ def intsum(n):
     count = 0
     for num in range(100, 1000):
         sum = 0
-        for digit in str(num):
-            sum += int(digit)
+        while num != 0:
+            last_digit = num % 10
+            num = num // 10
+            sum += last_digit
         if sum == n:
             count += 1
     return count
