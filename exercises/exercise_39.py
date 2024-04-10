881f08773cd0798c9f294e42e0544ca92a6ecbfb
Fadi-Mostefai <fadimostefai@gmail.com>
2024-04-10 18:57:26 +0100

    Fixed Exercise 39

--- exercises/exercise_39.py
+++ exercises/exercise_39.py
@@ -4,8 +4,10 @@ def digit(n):
     sum = 0
     if n < 0:
         n = n * -1
-    for digit in str(n):
-        sum += int(digit)
+    while n != 0:
+        last_digit = n % 10
+        n = n // 10
+        sum += last_digit
     return sum
 
 n = int(input())
