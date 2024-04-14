085664ef89e65cfef8f2c4b0de6d707d9bb0e3e8
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-04-14 19:06:10 +0100

    Update exercise_33.py

--- exercises/exercise_33.py
+++ exercises/exercise_33.py
@@ -1,5 +1,6 @@
 # Your solution to Exercise 33
-def table(n):
+def table(n):  # Function naming, parameter naming, and type hinting. 'print_table' is a better name.
+    '''  # Docstring missing. ''' # In such short functions, docstrings can help to understand the purpose of the function.
     for i in range(n):
         print(("-1\t" * i) + "0\t" + ("1\t" * (n-i-1)))
 
