53987cf3ba4b3053b6e542e082bb61c8416e6d76
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-04-14 17:48:53 +0100

    Update exercise_5.py
    Add comments

--- exercises/exercise_5.py
+++ exercises/exercise_5.py
@@ -8,3 +8,20 @@ def intsum(n):
 
 n = int(input())
 print(intsum(n))
+
+# Same issue here:
+# Function naming, parameter naming, and type hinting.
+
+# I would recommend to use naming such as "get_sum_of_integers" for the function
+# or "get_sum". In this case such a short name is appropriate and quite 
+# descriptive.
+
+# Every time you use "return" in a function, you should annotate the function
+# with the return type. In this case, the function is returning an integer, so
+# you should annotate the function with "-> int" to indicate that the function
+# is returning an integer.
+
+# Also, I recommend to use "get_" prefix for functions that are returning some
+# value, such as "get_sum_of_integers" or "get_sum". This is a common naming
+# convention in Python and makes it clear that the function is returning a
+# value.
