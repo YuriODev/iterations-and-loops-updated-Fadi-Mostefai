c42e826d2cbf8af5346ae4f7b96240c6ad2e2189
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-04-14 17:48:06 +0100

    Update exercise_12.py
    Add comments

--- exercises/exercise_12.py
+++ exercises/exercise_12.py
@@ -9,3 +9,22 @@ def sum(n):
 
 n = int(input())
 print(sum(n))
+
+# Same issue here:
+# Function naming, parameter naming, and type hinting.
+
+# A few important things to note here:
+# 1) You must be very careful with the naming of your functions and parameters.
+# Since you are using "sum" as a variable name, you should not use it as a
+# function or variable name, as it will overwrite the built-in sum function.
+
+# It worked out fine in this case, but it's a good practice to avoid using
+# built-in function names as variable names.
+
+# For example (error):
+# list = [1, 2, 3, 4, 5]
+# sum = 0
+# a = int(input())
+# list += a
+# sum += sum(list)
+# How many errors can you find in the above code? :D 
