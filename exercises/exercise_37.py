d828202d6505e7b1a9716338afa615fc51a1d36c
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-04-14 19:06:20 +0100

    Update exercise_37.py

--- exercises/exercise_37.py
+++ exercises/exercise_37.py
@@ -14,3 +14,22 @@ def div(a, b):
 a = int(input())
 b = int(input())
 print(div(a,b))
+
+# Same Issue:
+# Function naming, parameter naming, and type hinting
+
+# Example how to improve:
+# def division(dividend: int, divisor: int) -> str:
+#     """Returns the quotient and remainder of the division of two numbers."""
+#     remainder = 0
+#     count = 0
+#     while True:
+#         if dividend - divisor < 0:
+#             remainder = dividend
+#             break
+#         dividend -= divisor
+#         count += 1
+#     return f"{count} {remainder}"
+
+# Even if you make a mistake, it will be always easier to read proper names
+# rather than wasting your "mindfuel" what values are stored in a and b.
