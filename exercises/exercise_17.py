ec2b29938b86d71990218c2cd3b960336a87fe12
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-04-14 17:48:24 +0100

    Update exercise_17.py
    Add comments

--- exercises/exercise_17.py
+++ exercises/exercise_17.py
@@ -12,3 +12,9 @@ m = int(input())
 pattern(n,m)
 
 #print("0\t0\t0\t\n1\t1\t1\t\n2\t2\t2\t\n3\t3\t3\t\n4\t4\t4\t\n5\t5\t5\t\n")
+
+# Same issue here:
+# Function naming, parameter naming, and type hinting.
+
+# A few important things to note here:
+# Amazing approach to printing the pattern.
