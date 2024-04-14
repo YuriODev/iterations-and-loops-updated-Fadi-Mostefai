a379a40492c9a07bc3c88c96ab1b055e20631310
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-04-14 19:06:33 +0100

    Update exercise_43.py

--- exercises/exercise_43.py
+++ exercises/exercise_43.py
@@ -14,3 +14,27 @@ def sec_largest():
     return second_large
 
 print(sec_largest())
+
+# Same Issue:
+# Function naming, parameter naming, and type hinting
+
+# Note:
+# Better to avoid input() function in the function itself for future cases,
+# but I understand that you can't do it in this case.
+
+# Example how to improve:
+# deg get_second_largest() -> int:
+#     num = int(input())
+#     second_largest = 0
+#     largest = num
+#     while num != 0:
+#         num = int(input())
+#         if num > largest:
+#             if second_largest == 0:
+#                 second_largest = largest
+#             largest = num
+#         elif second_largest < num <= largest:
+#             second_largest = num
+#     return second_largest
+
+
