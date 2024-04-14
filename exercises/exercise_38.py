10bf335e79895e9a8c69b232024eb910766e42b4
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-04-14 19:06:23 +0100

    Update exercise_38.py

--- exercises/exercise_38.py
+++ exercises/exercise_38.py
@@ -21,3 +21,37 @@ def even_diff(n):
 
 n = int(input())
 print(even_diff(n))
+
+# Same Issue:
+# Function naming, parameter naming, and type hinting.
+
+# Notes:
+# 1. The function name and parameter name can be improved to:
+#   - `is_even_difference`
+# prefix "is" is used to indicate that the function returns a boolean value.
+# 2. Return statement can be used to avoid else block, same as before
+# 3. The final return statement can be simplified to:
+# return (large - small) % 2 == 0 
+# since the condition is already a boolean expression.
+
+# Example of refactored code:
+# def is_even_difference(number: int) -> bool:
+#     """Returns True if the difference between the largest and smallest digit of a number is even, otherwise False."""
+#     largest_digit = 0
+#     smallest_digit = 0
+#     if number == 0:
+#         return False
+#     while number != 0:
+#         last_digit = number % 10
+#         number //= 10
+#         if last_digit > largest_digit:
+#             if smallest_digit == 0:
+#                 smallest_digit = largest_digit
+#             largest_digit = last_digit
+#         elif smallest_digit == 0:
+#             smallest_digit = last_digit
+#         elif last_digit < smallest_digit:
+#             smallest_digit = last_digit
+#     return (largest_digit - smallest_digit) % 2 == 0
+
+# Also, you can see the use of "//=" operator to perform floor division in the code.
